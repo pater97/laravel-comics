6fcc9f6ed5a3fafd585d3aa70d7df6848f2886ed
pater97 <giuseppepater997@gmail.com>
2021-12-17 18:21:33 +0100

    sezione con card completata

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,21 +5,24 @@
 <!-- qui si inserisce il main dell'homepage -->
 <section class="homepage">
     <div class="hero_image"></div>
-</section>
-<section class="container-fluid">
-    <div class="container card_container">
-        <div class="row g-4">
-        @foreach($cards as $card)
-            <div class="col-md-2">
-                <div class="card">
-                    <img src="{{$card['thumb']}}" alt="">
-                    <h5>
-                    {{$card['series']}}
-                    </h5>
-                </div>
+    <section class="container-fluid">
+        <div class="container card_container">
+            <div class="current_series brand_bk">
+                CURRENT SERIES
             </div>
-            @endforeach
+            <div class="row g-4">
+                @foreach($cards as $card)
+                <div class="col-md-2">
+                    <div class="card">
+                        <img src="{{$card['thumb']}}" alt="">
+                        <h5>
+                            {{strtoupper($card['series'])}}
+                        </h5>
+                    </div>
+                </div>
+                @endforeach
+            </div>
         </div>
-    </div>
+    </section>
 </section>
 @endsection
