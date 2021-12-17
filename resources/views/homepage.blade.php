09f6fc0ee76401f6c03e0392dcb34ad6cca7f6f0
pater97 <giuseppepater997@gmail.com>
2021-12-17 17:56:56 +0100

    inserimento dinamico tramite "database" delle card nel main

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,4 +3,23 @@
 
 @section('content')
 <!-- qui si inserisce il main dell'homepage -->
+<section class="homepage">
+    <div class="hero_image"></div>
+</section>
+<section class="container-fluid">
+    <div class="container card_container">
+        <div class="row g-4">
+        @foreach($cards as $card)
+            <div class="col-md-2">
+                <div class="card">
+                    <img src="{{$card['thumb']}}" alt="">
+                    <h5>
+                    {{$card['series']}}
+                    </h5>
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+
+    $cards = config('comics_db.cards');
+    return view('homepage',compact('cards'));
 });
