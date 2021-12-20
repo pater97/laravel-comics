cd6c6f91fbabb9b3045b52dbee2b57b5c6616b7b
pater97 <giuseppepater997@gmail.com>
2021-12-20 10:29:53 +0100

    main compleatato

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,7 +5,7 @@
 <!-- qui si inserisce il main dell'homepage -->
 <section class="homepage">
     <div class="hero_image"></div>
-    <section class="container-fluid">
+    <section class="container-fluid card_section">
         <div class="container card_container">
             <div class="current_series brand_bk">
                 CURRENT SERIES
@@ -21,6 +21,47 @@
                     </div>
                 </div>
                 @endforeach
+            </div>
+        </div>
+        <div class="d-flex justify-content-center pb-5">
+            <div class="button brand_bk py-2 px-4 fw-bold">
+                LOAD MORE
+            </div>
+        </div>
+    </section>
+    <section class="blue_option container-fluid brand_bk">
+        <div class="container">
+            <div class="row">
+                <div>
+                    <img src="../../img/buy-comics-digital-comics.png" alt="dc phone">
+                    <h5>
+                        DIGITAL COMICS
+                    </h5>
+                </div>
+                <div>
+                    <img src="../../img/buy-comics-merchandise.png" alt="dc phone">
+                    <h5>
+                        DC MERCHANDISE
+                    </h5>
+                </div>
+                <div>
+                    <img src="../../img/buy-comics-subscriptions.png" alt="dc phone">
+                    <h5>
+                        SUBSCRIPTION
+                    </h5>
+                </div>
+                <div>
+                    <img src="../../img/buy-comics-shop-locator.png" alt="dc phone">
+                    <h5>
+                        COMIC SHOP LOCATOR
+                    </h5>
+                </div>
+                <div>
+                    <img src="../../img/buy-comics-subscriptions.png" alt="dc phone">
+                    <h5>
+                        DC POWER VISA
+                    </h5>
+                </div>
             </div>
         </div>
     </section>
