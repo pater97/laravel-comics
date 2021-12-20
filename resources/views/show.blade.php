399e5073863c1c653e9747f44d3c8e54b1593fed
pater97 <giuseppepater997@gmail.com>
2021-12-20 21:51:06 +0100

    esercizio terminato

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,6 +5,7 @@
 
 @section('content')
 <section id="show_page">
+    <!-- sezione con anteprima cover e jumbotron -->
     <div class="hero_image">
         <div class="container position-relative">
             <div class="card_section position-absolute">
@@ -20,25 +21,23 @@
             </div>
         </div>
     </div>
+    <!-- separatore blue4  -->
     <div class="blue_band brand_bk">
     </div>
-    <div class="container description d-flex">
+    <!-- sezione con descrizione e barra verde del prezzo  -->
+    <div class="container description d-flex py-5">
         <div class="col-md-9">
-            <h1>
+            <h1 class="blue_text">
                 {{$comic['title']}}
             </h1>
             <div class="green_bar">
                 <div class="col-9">
-                    <div>
                         <h5 class="px-3">
                             <span>U.S Price:</span>  {{$comic['price']}}
                         </h5>
-                    </div>
-                    <div>
                         <h5 class="px-5">
                             <span>AVAILABLE</span>
                         </h5>
-                    </div>
                 </div>
                 <div class="col-3">
                     <h5>
@@ -46,11 +45,116 @@
                     </h5>
                 </div>
             </div>
+            <div class="col-12 py-3 text_descriton">
+                     <p>{{$comic['description']}}</p>
+            </div>
+        </div>
+        <div class="col-3 ad">
+            <h5>ADVERSIMENT</h5>
+            <img src="../../img/adv.jpg" alt="">
+        </div>
+    </div>
+    <!-- info extra con scrittori e artisti  -->
+    <div class=" extra_info gray_container  container-fluid pt-4">
+        <div class="container d-flex justify-content-between ">
+            <div class="col-md-6 pe-3">
+                <div class="talent border-bottom">
+                    <h2 class="blue_text">
+                        Talent
+                    </h2>
+                </div>
+                <div class="art border-bottom d-flex justify-content-between py-2">
+                    <div class="left col-3">
+                        <h5 class="blue_text">
+                            Art by:
+                        </h5>
+                    </div>
+                    <div class="right col-9">
+                       @for($i=0;$i < count($comic['artists']);$i++)
+                        {{$comic['artists'][$i]}},
+                        @endfor
+                    </div>
+                </div>
+                <div class="writer border-bottom d-flex justify-content-between py-2">
+                    <div class="left col-3">
+                        <h5 class="blue_text">
+                            Written by:
+                        </h5>
+                    </div>
+                    <div class="right col-9">
+                       @for($i=0;$i < count($comic['writers']);$i++)
+                        {{$comic['writers'][$i]}},
+                        @endfor
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6 ps-3">
+                <div class="specs border-bottom">
+                    <h2 class="blue_text">
+                        Specs
+                    </h2>
+                </div>
+                <div class="series border-bottom d-flex justify-content-between py-2">
+                    <div class="left col-3">
+                        <h5 class="blue_text">
+                            Series:
+                        </h5>
+                    </div>
+                    <div class="right col-9">
+                       {{$comic['series']}}
+                    </div>
+                </div>
+                <div class="price border-bottom d-flex justify-content-between py-2">
+                    <div class="left col-3">
+                        <h5 class="blue_text">
+                            U.S price:
+                        </h5>
+                    </div>
+                    <div class="right col-9">
+                       {{$comic['price']}}
+                    </div>
+                </div>
+                <div class="date border-bottom d-flex justify-content-between py-2">
+                    <div class="left col-3">
+                        <h5 class="blue_text">
+                            On sale date:
+                        </h5>
+                    </div>
+                    <div class="right col-9">
+                       {{$comic['sale_date']}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- sezione con icone genereiche grigio chiaro  -->
+    <div class="icon_band gray_container container-fluid">
+        <div class="container d-flex">
+            <div class="col-md-3">
+                <h5>
+                    DIGITAL COMICS
+                </h5>
+                <i class="fas fa-mobile-alt"></i>
+            </div>
+            <div class="col-md-3">
+                <h5>
+                    SHOP DC
+                </h5>
+                <i class="fas fa-credit-card"></i>
+            </div>
+            <div class="col-md-3">
+                <h5>
+                    COMIC SHOP LOCATOR
+                </h5>
+                <i class="fas fa-map-marker-alt"></i>
+            </div>
+            <div class="col-md-3">
+                <h5>
+                    SUBSCRITION
+                </h5>
+                <i class="fas fa-tshirt"></i>
+            </div>
         </div>
     </div>
 </section>
-<!-- <div>
-    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}" />
-    <p>{{ $comic['series'] }}</p>
-</div> -->
 @endsection
